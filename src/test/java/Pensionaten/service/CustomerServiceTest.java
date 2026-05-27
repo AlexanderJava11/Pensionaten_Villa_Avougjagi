c59192c8e46025fbd953b3e9843b7d07c68efd9b
Boris Ivis <boris.ivis@hotmail.com>
2026-05-28 01:24:54 +0200

    CustomerServiceTest fixat

--- src/test/java/Pensionaten/service/CustomerServiceTest.java
+++ src/test/java/Pensionaten/service/CustomerServiceTest.java
@@ -9,12 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.List;
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+
 
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
