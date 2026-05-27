f51668dcbbf46f9f9b9868147bc8a7a258305a6c
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-27 11:50:30 +0200

    Förbättrat hotel javascript koden samt struktur och lagt in kommentarer

--- src/main/resources/static/js/hotel.js
+++ src/main/resources/static/js/hotel.js
@@ -1,11 +1,22 @@
+// Vöntar tills hela HTML sidan har laddats klart
 document.addEventListener("DOMContentLoaded", function () {
+
+  // Hämtar alla länkar som har attributet data confirm
   const confirmLinks = document.querySelectorAll("[data-confirm]");
 
+  // Loppar igenom alla länkar
   confirmLinks.forEach(function (link) {
+
+    // Lyssnar efter klick på länk
     link.addEventListener("click", function (event) {
+
+      // Hämtar meddelandet från data confrim attributet
       const message = link.dataset.confirm;
 
+      // Visar bekräftelseruta innan åtgärden utförs
       if (!window.confirm(message)) {
+
+        // Stoppar länken om användaren trycket på Avbryt
         event.preventDefault();
       }
     });
