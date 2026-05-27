4860bd8583b156ae236a115dea97fdeedef8a0fb
Boris Ivis <boris.ivis@hotmail.com>
2026-05-28 01:23:24 +0200

    Separat test som inte använder mockito

--- src/test/java/Pensionaten/service/BookingServiceTests.java
+++ src/test/java/Pensionaten/service/BookingServiceTests.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
@@ -19,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Rollback
 class BookingServiceTests {
 
     @Autowired
