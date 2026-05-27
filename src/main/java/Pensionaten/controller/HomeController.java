9397460718484c287d068304f429bbfaf11fd7dd
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-27 12:49:19 +0200

    Lagt till kommentarer i HomeController

--- src/main/java/Pensionaten/controller/HomeController.java
+++ src/main/java/Pensionaten/controller/HomeController.java
@@ -4,12 +4,18 @@ import Pensionaten.dto.BookingDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// Controller för startsidan
 @Controller
 public class HomeController {
 
+    // Hanterar både "/" och "/home"
     @GetMapping({"/", "/home"})
     public String home(org.springframework.ui.Model model) {
+
+        // Skapar ett tomt BookingDTO objekt som används i sökformuläret på startsidan
         model.addAttribute("booking", new BookingDTO());
+
+        // Returnerar startsidan home.html
         return "customers/home";
     }
 }
